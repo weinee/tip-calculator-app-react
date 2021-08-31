932cbe8a6e460020a4279ab75938dda00e40ac78
weinee <weinee24@gmail.com>
2021-08-31 14:17:52 +0800

    Right container

--- src/Components/Right.js
+++ src/Components/Right.js
@@ -3,10 +3,25 @@ import React from "react";
 const Right = () => {
   return (
     <div className="right-container">
-      <text>Tip Amount</text>
-      <small>/person</small>
-      <text>Total</text>
-      <small>/person</small>
+      <div className="row">
+        <div>
+          <text>Tip Amount</text>
+          <br />
+          <small>/person</small>
+        </div>
+        <h1>$0.00</h1>
+      </div>
+
+      <div className="row">
+        <div>
+          <text>Total</text>
+          <br />
+          <small>/person</small>
+        </div>
+        <h1>$0.00</h1>
+      </div>
+
+      <button onClick={() => {}}>RESET</button>
     </div>
   );
 };
